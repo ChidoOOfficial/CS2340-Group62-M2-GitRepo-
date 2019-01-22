459419597fd40be7d1e941333207c3cff2163668
BuildTools <unconfigured@null.spigotmc.org>
2019-01-22 12:51:59 -0500

    I implemented the calc method in the class.

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,11 +1,12 @@
 package edu.gatech.oad.antlab.person;
+import java.util.Random;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
+ * @author Charles
  * @version 1.1
  */
 public class Person2 {
@@ -31,7 +32,17 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+		Random rand = new Random();
+		String a = input;
+		char b;
+		int random = rand.nextInt(input.length() - 1) + 1;
+		for (int i = 0; i < input.length(); i++) {
+			b = input.charAt(random);
+			a += b;
+
+		}
+
+	  return a;
 	}
 	/**
 	 * Return a string rep of this object
